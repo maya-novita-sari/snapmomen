f78e224e2fd7cca305d1b5f4c3d278ae5ebcdba2
takara-kairimei <mayanovita0sari@gmail.com>
2026-09-22 18:28:01 +0700

    fix config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,4 +1,4 @@
 
-const API_BASE_URL = 'https://snapmomen-git-main-takara-kairimeis-projects.vercel.app/';
+const API_BASE_URL = 'https://snapmomen.vercel.app/';
 
 const PHOTO_LIFETIME_LABEL = '3 hari';
